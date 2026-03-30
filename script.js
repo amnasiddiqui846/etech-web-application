ea7ed3f69c55007cbd8667836a0cf13109c0be41
amna siddiqui <amnasiddiqui846@gmail.com>
2026-03-30 10:15:22 +0500

    update

---
+++ script.js
@@ -0,0 +1,15 @@
+function btn(){
+        alert("Your Enroll in this Course")
+}
+
+function btn1(){
+        alert("Your successfully subscribe this E-Learn Website")
+}
+
+function btn2(){
+        alert("Your Registration is complete now plz go to Login page")
+}
+
+function btn3(){
+        alert("Your successfully login in this page")
+}
